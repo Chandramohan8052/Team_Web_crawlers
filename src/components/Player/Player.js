c8a654b57f45970e0d6404755db6c7bd3b131809
thechandramohan <cm5674713@gmail.com>
2023-03-31 22:52:45 +0530

    changes

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,70 +1,29 @@
-import React, { useState, useRef, useEffect } from "react";
-import Controls from "./Controls";
-import Details from "./Details";
-import './Player.css';
+import React from 'react'
 
-function Player(props) {
-  const audioEl = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+export default function Player() {
+    return (
+        <div className='c-main' >
+            <div className='c-player' >
+                <div className='box-img'>
+                    <img src="https://uploads-ssl.webflow.com/6206088de5b0708e27001c97/6394eea1831ce26d9ba448ad_binaural-beats.png" alt="" />
 
-  useEffect(() => {
-    if (isPlaying) {
-      audioEl.current.play();
-    } else {
-      audioEl.current.pause();
-    }
-  });
+                </div>
+                <button class="button-20" role="button" >Binural Beats</button>
+                {/* <h4 className='box-title'>Binural Beats</h4> */}
+            </div>
 
-  const SkipSong = (forwards = true) => {
-    if (forwards) {
-      props.setCurrentSongIndex(() => {
-        let temp = props.currentSongIndex;
-        temp++;
+            <div className='c-player' >
+                <div className='box-img'>
+                    <img src="https://www.108.house/wp-content/uploads/2021/11/imagen-blog-saty-mantras-1024x711.jpeg" alt="" />
 
-        if (temp > props.songs.length - 1) {
-          temp = 0;
-        }
+                </div>
+                <button class="button-20" role="button" >Mantras Meditation</button>
+                {/* <button className='box-button'>Mantras Meditation</button> */}
+                {/* <h4 className='box-title'>Mantras Meditaion</h4> */}
+            </div>
 
-        return temp;
-      });
-    } else {
-      props.setCurrentSongIndex(() => {
-        let temp = props.currentSongIndex;
-        temp--;
 
-        if (temp < 0) {
-          temp = props.songs.length - 1;
-        }
+        </div>
 
-        return temp;
-      });
-    }
-  };
-
-  return (
-    <div className="c-player">
-      <h4>Playing now</h4>
-      <Details song={props.songs[props.currentSongIndex]} />
-      <Controls
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        SkipSong={SkipSong}
-      />
-      <audio
-        className="c-player--audio"
-        src={props.songs[props.currentSongIndex].src}
-        ref={audioEl}
-        controls
-      ></audio>
-      <p>
-        Next up:{" "}
-        <span>
-          {props.songs[props.nextSongIndex].title} by{" "}
-          {props.songs[props.nextSongIndex].artist}
-        </span>
-      </p>
-    </div>
-  );
+    )
 }
-
-export default Player;
